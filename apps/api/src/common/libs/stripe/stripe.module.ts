c1769c71dad1baac1b8537007f16c931a4de64d5
Eugene <sifmeop@gmail.com>
2026-03-06 17:11:21 +0200

    fix: remove unused module

--- apps/api/src/common/libs/stripe/stripe.module.ts
+++ apps/api/src/common/libs/stripe/stripe.module.ts
@@ -3,14 +3,13 @@ import { Global, Module } from '@nestjs/common'
 import { MailModule } from '../mail/mail.module'
 
 import { StripeController } from './stripe.controller'
-import { StripeResolver } from './stripe.resolver'
 import { StripeService } from './stripe.service'
 
 @Global()
 @Module({
   imports: [MailModule],
   controllers: [StripeController],
-  providers: [StripeResolver, StripeService],
+  providers: [StripeService],
   exports: [StripeService]
 })
 export class StripeModule {}
